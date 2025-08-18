338fb07ce85584ed5044c61e60b473d873fff409
roudramondal <mondalroudra212@gmail.com>
2025-08-18 07:58:57 +0600

    using push function sum of all vector

--- Module-2/partial-sum/sum_all_array.cpp
+++ Module-2/partial-sum/sum_all_array.cpp
@@ -22,6 +22,57 @@
    Problem Link: 
 */
 
+
+// using vector cin use
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+// #define nl '\n'
+// #define YES cout << 'YES\n'
+// #define Yes cout << 'Yes\n'
+// #define NO cout << 'NO\n'
+// #define No cout << 'No\n'
+// #define ld long double
+// #define pb push_back
+// #define po pop_back
+// #define all(v) v.begin(), v.end()
+// #define pii pair<int,int>
+// #define pll pair<<ll ,ll>
+
+// #define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// void solve() {
+//     // code here 
+//     int n;
+//     cin>>n;
+//     int sum =0;
+//     vector<int>arr(n);
+//     for (int i=0;i<n;i++){
+//         cin>>arr[i];
+//         sum+= arr[i];
+//     }
+//     cout<<sum<<nl;
+// }
+
+// int main() {
+//     fast_io;
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         solve();
+//     }
+//     return 0;
+// }
+
+// using vector push back
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 08 / 18 / 2025
+   Time  : 07:56
+   Problem Link: 
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -45,10 +96,12 @@ void solve() {
     int n;
     cin>>n;
     int sum =0;
-    vector<int>arr(n);
+    vector<int>arr;
     for (int i=0;i<n;i++){
-        cin>>arr[i];
-        sum+= arr[i];
+        int x;
+        cin>>x;
+        arr.pb(x);
+        sum+=arr[i];
     }
     cout<<sum<<nl;
 }
