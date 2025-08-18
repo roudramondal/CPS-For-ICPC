b6fa395dc726364d9c76469ca64fdd3d3bca12fc
roudramondal <mondalroudra212@gmail.com>
2025-08-18 07:51:27 +0600

    using vector sum of all array

--- Module-2/partial-sum/sum_all_array.cpp
+++ Module-2/partial-sum/sum_all_array.cpp
@@ -1,17 +1,64 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main ()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for (int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int sum=0;
+//     for (int i=0;i<n;i++){
+//         sum+=arr[i];
+//     }
+//     cout<<sum<<endl;
+//     return 0;
+// }
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 08 / 18 / 2025
+   Time  : 07:47
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-int main ()
-{
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int,int>
+#define pll pair<<ll ,ll>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    // code here 
     int n;
     cin>>n;
-    int arr[n];
+    int sum =0;
+    vector<int>arr(n);
     for (int i=0;i<n;i++){
         cin>>arr[i];
+        sum+= arr[i];
     }
-    int sum=0;
-    for (int i=0;i<n;i++){
-        sum+=arr[i];
+    cout<<sum<<nl;
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
     }
-    cout<<sum<<endl;
     return 0;
 }
